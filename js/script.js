8af83c29568785b04ab422aaeb8ab1a16ec59261
Anton Pomazkov <vitrumbeta@gmail.com>
2021-05-28 14:27:57 +0800

    Подключил Swiper к слайдеру

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Слайдер (Swiper)
+
+const slider = document.querySelector(".swiper-container");
+
+let mySwiper = new Swiper(slider, {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
